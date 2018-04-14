273d11856d90245b4d0e09ea51857e9423aad74b
Joao Souza <jvms3d@gmail.com>
2018-04-14 17:58:45 -0300

    Json endpoint /pedidos/{id}

--- src/main/java/com/joaosouza/cursomc/domain/Cliente.java
+++ src/main/java/com/joaosouza/cursomc/domain/Cliente.java
@@ -16,6 +16,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.joaosouza.cursomc.domain.enums.TipoCliente;
 
@@ -39,6 +40,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
---
+++ src/main/java/com/joaosouza/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.joaosouza.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaosouza.cursomc.domain.Pedido;
+import com.joaosouza.cursomc.repositories.PedidoRepository;
+import com.joaosouza.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
