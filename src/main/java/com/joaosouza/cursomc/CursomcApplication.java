3ad7356a38512dc455eed77782f34d27e85a87e1
Joao Souza <jvms3d@gmail.com>
2018-04-14 15:34:23 -0300

    Mapeamento Pedido, Pagamento e EstadoPagamento

--- src/main/java/com/joaosouza/cursomc/CursomcApplication.java
+++ src/main/java/com/joaosouza/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.joaosouza.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.joaosouza.cursomc.domain.Cidade;
 import com.joaosouza.cursomc.domain.Cliente;
 import com.joaosouza.cursomc.domain.Endereco;
 import com.joaosouza.cursomc.domain.Estado;
+import com.joaosouza.cursomc.domain.Pagamento;
+import com.joaosouza.cursomc.domain.PagamentoComBoleto;
+import com.joaosouza.cursomc.domain.PagamentoComCartao;
+import com.joaosouza.cursomc.domain.Pedido;
 import com.joaosouza.cursomc.domain.Produto;
+import com.joaosouza.cursomc.domain.enums.EstadoPagamento;
 import com.joaosouza.cursomc.domain.enums.TipoCliente;
 import com.joaosouza.cursomc.repositories.CategoriaRepository;
 import com.joaosouza.cursomc.repositories.CidadeRepository;
 import com.joaosouza.cursomc.repositories.ClienteRepository;
 import com.joaosouza.cursomc.repositories.EnderecoRepository;
 import com.joaosouza.cursomc.repositories.EstadoRepository;
+import com.joaosouza.cursomc.repositories.PagamentoRepository;
+import com.joaosouza.cursomc.repositories.PedidoRepository;
 import com.joaosouza.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -89,5 +101,21 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2018 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2018 10:32"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 }
--- src/main/java/com/joaosouza/cursomc/domain/Pagamento.java
+++ src/main/java/com/joaosouza/cursomc/domain/Pagamento.java
@@ -2,14 +2,28 @@ package com.joaosouza.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
 import com.joaosouza.cursomc.domain.enums.EstadoPagamento;
 
-public class Pagamento implements Serializable {
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
+public abstract class Pagamento implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private Integer id;
-	private EstadoPagamento estado;
+	private Integer estado;
 	
+	@OneToOne
+	@JoinColumn(name="pedido_id")
+	@MapsId
 	private Pedido pedido;
 	
 	public Pagamento() {
@@ -18,7 +32,7 @@ public class Pagamento implements Serializable {
 	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
 		super();
 		this.id = id;
-		this.estado = estado;
+		this.estado = estado.getCodigo();
 		this.pedido = pedido;
 	}
 
@@ -31,11 +45,11 @@ public class Pagamento implements Serializable {
 	}
 
 	public EstadoPagamento getEstado() {
-		return estado;
+		return EstadoPagamento.toEnum(estado);
 	}
 
 	public void setEstado(EstadoPagamento estado) {
-		this.estado = estado;
+		this.estado = estado.getCodigo();
 	}
 
 	public Pedido getPedido() {
