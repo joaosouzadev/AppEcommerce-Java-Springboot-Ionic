babc8509dc03d5833b002603e0f37c84f9fdccaa
Joao Souza <jvms3d@gmail.com>
2018-04-14 15:10:54 -0300

    Criacao classes Pedido, Pagamento e subclasses

--- src/main/java/com/joaosouza/cursomc/domain/Cliente.java
+++ src/main/java/com/joaosouza/cursomc/domain/Cliente.java
@@ -39,6 +39,8 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 	}
 
@@ -51,8 +53,6 @@ public class Cliente implements Serializable {
 		this.tipo = tipo.getCodigo();
 	}
 
-	
-	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -132,6 +132,14 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 	
 	
