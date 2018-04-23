5eb0f4a5d317a49ea25a278ade4578b662aa9631
Joao Souza <jvms3d@gmail.com>
2018-04-22 23:58:20 -0300

    Validacao customizada para email repetido na atualizacao de Cliente

--- src/main/java/com/joaosouza/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/joaosouza/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.joaosouza.cursomc.domain.Cliente;
+import com.joaosouza.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
